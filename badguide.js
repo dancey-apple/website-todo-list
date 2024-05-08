ef0909d5291b42012b234a9ec7112c1405eacde1
dancey-apple <dancey.apple@proton.me>
2024-05-08 09:59:58 -0500

    Starting from scratch. Renamed bad code to 'badguide' to use as referance

---
+++ badguide.js
@@ -0,0 +1,56 @@
+const todoValue = document.getElementById('todoText');
+const todoAlert = document.getElementById('Alert');
+const listItems = document.getElementById('list-items');
+const addUpdate = document.getElementById('AddUpdateClick');
+
+let todo = JSON.parse(localStorage.getItem('todo-list'));
+if (!todo) {
+    todo = [];
+}
+
+function createToDoItem(){
+    if (todoValue.value === "" || todoValue.value === null){
+        todoAlert.innerText = "Uhm.... you didn't add anything.";
+        todoValue.focus();
+    }else {
+        let isPresent = false;
+        todo.forEach((element) => {
+            if (element.item == todoValue.value){
+                isPresent = true;
+            }
+        });
+        
+        if (isPresent){
+            setAlertMessage("You alreay added this item, dude. -_-");
+            return;
+        }
+
+        let li = document.createElement('li');
+        const todoItems = `<div title="Hit Double Click and Complete" ondblclik='CompletedToDoItems(this)">${todoValue.value}</div><div><button class="edit todo-controls" onclick='updateToDoItems(this)'>Edit</button><button class='delete todo-controls' onclick='deleteTodoItems(this)'>Delete</button></div></div>`;
+        li.innerHTML = todoItems;
+        listItems.appendChild(li);
+
+        if (!todo){
+            todo = [];
+        }
+        let itemList = { item: todoValue.value, status: false};
+        todo.push(itemList);
+        setLocalStorage();
+    }
+    todoValue.value = "";
+    setAlertMessage("You added a new todo item. Get to work!");
+    //console.log(todoValue);
+}
+
+function setLocalStorage(){
+    localStorage.setItem('todo-list', JSON.stringify(todo));
+}
+
+function setAlertMessage(message){
+    todoAlert.removeAttribute("class");
+    todoAlert.innterText = message;
+    setTimeout(() => {
+        todoAlert.classList.add("toggleMe");
+    }, 1000);
+}
+
